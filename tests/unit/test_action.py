157a6870cf1e8b579fe211eacd5980b94719576d
Antonio Francisco <ajoaoff@gmail.com>
2022-04-08 16:10:21 -0300

    Move test_action from unittest to pytest

--- tests/unit/test_action.py
+++ tests/unit/test_action.py
@@ -1,72 +1,60 @@
 """Test Action abstraction for v0x01 and v0x04."""
-import unittest
-
-from pyof.foundation.basic_types import UBInt16, UBInt32
+import pytest
+from pyof.foundation.basic_types import UBInt32
 
 from napps.kytos.of_core.v0x01.flow import Action as Action01
 from napps.kytos.of_core.v0x04.flow import Action as Action04
 
 
-class TestAction(unittest.TestCase):
-    """Tests for the Action class."""
-
-    def test_all_actions(self):
-        """Test all action fields from and to dict."""
-        action_dicts = [
+@pytest.mark.parametrize(
+    'action_class, action_dict', [
+        [
+            Action01,
             {
-                'tests': [Action01, Action04],
-                'action': {
-                    'action_type': 'output',
-                    'port': UBInt32(1),
-                },
+                'action_type': 'output',
+                'port': UBInt32(1),
             },
+        ],
+        [
+            Action04,
             {
-                'tests': [Action01, Action04],
-                'action': {
-                    'action_type': 'set_vlan',
-                    'vlan_id': UBInt16(2),
-                },
+                'action_type': 'output',
+                'port': UBInt32(1),
             },
+        ],
+        [
+            Action04,
             {
-                'tests': [Action04],
-                'action': {
-                    'action_type': 'set_queue',
-                    'queue_id': UBInt32(4),
-                }
-            },
+                'action_type': 'set_queue',
+                'queue_id': UBInt32(4),
+            }
+        ],
+        [
+            Action04,
             {
-                'tests': [Action04],
-                'action': {
-                    'action_type': 'pop_vlan',
-                }
-            },
+                'action_type': 'pop_vlan',
+            }
+        ],
+        [
+            Action04,
             {
-                'tests': [Action04],
-                'action': {
-                    'action_type': 'push_vlan',
-                    'tag_type': 'c',
-                }
-            },
+                'action_type': 'push_vlan',
+                'tag_type': 'c',
+            }
+        ],
+        [
+            Action04,
             {
-                'tests': [Action04],
-                'action': {
-                    'action_type': 'push_vlan',
-                    'tag_type': 's',
-                }
-            },
-        ]
-        for action_class in Action01, Action04:
-            with self.subTest(action_class=action_class):
-                for action_test in action_dicts:
-                    if action_class not in action_test['tests']:
-                        continue
-                    expected = action_test['action']
-                    with self.subTest(expected=expected):
-                        action = action_class.from_dict(expected)
-                        actual = action.as_dict()
-                        of_action = action.as_of_action()
-                        self.assertDictEqual(expected, actual)
-                        self.assertDictEqual(
-                            expected,
-                            action_class.from_of_action(of_action).as_dict()
-                        )
+                'action_type': 'push_vlan',
+                'tag_type': 's',
+            }
+        ],
+    ]
+)
+def test_all_actions(action_class, action_dict):
+    """Test all action fields from and to dict."""
+    action = action_class.from_dict(action_dict)
+    actual = action.as_dict()
+    of_action = action.as_of_action()
+    assert action_dict == actual
+    assert action_dict == action_class.from_of_action(of_action).as_dict()
