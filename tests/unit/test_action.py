03395b05c2265f2a32237dc4245a910eafe94180
Vinicius Arcanjo <vindasil@fiu.edu>
2022-11-15 12:53:53 -0300

    Covered with unit tests

--- tests/unit/test_action.py
+++ tests/unit/test_action.py
@@ -1,8 +1,12 @@
 """Test Action abstraction for v0x04."""
+from unittest.mock import MagicMock
+
 import pytest
 from pyof.foundation.basic_types import UBInt32
 
 from napps.kytos.of_core.v0x04.flow import Action as Action04
+
+# pylint: disable=protected-access,unnecessary-lambda-assignment
 
 
 @pytest.mark.parametrize(
@@ -50,3 +54,19 @@ def test_all_actions(action_class, action_dict):
     of_action = action.as_of_action()
     assert action_dict == actual
     assert action_dict == action_class.from_of_action(of_action).as_dict()
+
+
+def test_add_action_class():
+    """Test add_action_class."""
+    name, new_class = "some_class", MagicMock()
+    Action04.add_action_class(name, new_class)
+    assert Action04._action_class[name] == new_class
+
+
+def test_add_experimenter_classes():
+    """Test add_experimenter_classes."""
+    resp = MagicMock()
+    experimenter, func = 0xff000002, lambda body: resp
+    Action04.add_experimenter_classes(experimenter, func)
+    assert Action04._experimenter_classes[experimenter] == func
+    assert Action04.get_experimenter_class(experimenter, b'\xff') == resp
